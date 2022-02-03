22c30264228c1d1024795250b9fd79885743ef74
Adam Hitchcock <adam@northisup.com>
2022-02-03 07:57:05 +0000

    stuff

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="tree-sitter-python-language",
-    version="0.0.5",
+    version="0.0.6",
     packages=find_packages(),
     requires=["tree_sitter", "requests"],
 )
