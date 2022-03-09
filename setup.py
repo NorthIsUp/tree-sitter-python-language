28ead86a9446384daff51e965b5cd04f51c393e2
Adam Hitchcock <adam@northisup.com>
2022-03-09 14:11:06 -0800

    compile as a part of install

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
+from distutils.command.install import install
 from distutils.core import setup
 from pathlib import Path
 from typing import Any, Dict
@@ -24,6 +25,17 @@ package_data = {
 
 requirements = ["tree_sitter>=0.20.0,<0.21.0"]
 
+
+class _install(install):
+    def run(self):
+        install.run(self)
+
+        print("Building python-language.so", file=sys.stderr)
+        from tree_sitter_python_language import build_python_language
+
+        build_python_language(rebuild=True)
+
+
 setup_kwargs: Dict[str, Any] = {
     "name": package_name,
     "version": __version__,
@@ -38,6 +50,7 @@ setup_kwargs: Dict[str, Any] = {
     "package_data": package_data,
     "install_requires": requirements,
     "python_requires": ">=3.7,<4.0",
+    "cmdclass": {"install": _install},
 }
 
 setup(**setup_kwargs)
